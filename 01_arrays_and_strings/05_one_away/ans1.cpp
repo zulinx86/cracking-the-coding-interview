2d5f8cac3b89768ac8f2626af741ddedbb648a60
zulinx86 <zulinx86@gmail.com>
2021-07-20 05:49:32 +0000

    refactoring

--- 01_arrays_and_strings/05_one_away/ans1.cpp
+++ 01_arrays_and_strings/05_one_away/ans1.cpp
@@ -1,49 +1,50 @@
 /*
- * Check separately
- * Time complexity: O(n)
+ * N: the length of the longer string in the given strings
+ * Time complexity: O(N)
  * Space complexity: O(1)
  */
 
-#include<iostream>
-#include<string>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-class Solution {
-public:
-	bool oneAway(const string &s1, const string& s2) {
-		const string *t1, *t2;
-		if (s1.length() > s2.length()) t1 = &s1, t2 = &s2;
-		else t1 = &s2, t2 = &s1;
+bool oneAway(const string &s1, const string& s2) {
+	const string *t1, *t2;
+	if (s1.length() > s2.length())
+		t1 = &s1, t2 = &s2;
+	else
+		t1 = &s2, t2 = &s1;
 
-		if (t1->length() - t2->length() > 1) return false;
+	if (t1->length() - t2->length() > 1)
+		return false;
 
-		if (t1->length() == t2->length()) {
-			int cnt = 0;
-			for (int i = 0; i < t1->length(); ++i) {
-				if ((*t1)[i] != (*t2)[i]) ++cnt;
-				if (cnt > 1) return false;
-			}
-			return true;
-		}
-
-		bool hasDiff = false;
-		for (int i = 0; i < t2->length(); ++i) {
-			if (hasDiff) {
-				if ((*t1)[i+1] != (*t2)[i]) return false;
-			}
-			else {
-				if ((*t1)[i] != (*t2)[i]) hasDiff = true;
+	if (t1->length() == t2->length()) {
+		int cnt = 0;
+		for (int i = 0; i < t1->length(); ++i) {
+			if ((*t1)[i] != (*t2)[i]) {
+				++cnt;
+				if (cnt > 1)
+					return false;
 			}
 		}
-
 		return true;
 	}
-};
+
+	bool hasDiff = false;
+	for (int i = 0; i < t2->length(); ++i) {
+		if (hasDiff && (*t1)[i+1] != (*t2)[i])
+				return false;
+		else if ((*t1)[i] != (*t2)[i])
+				hasDiff = true;
+	}
+
+	return true;
+}
+
 
 int main(void) {
-	Solution solution;
 	vector<pair<string, string>> inputs({
 		{"pale", "ple"},
 		{"pales", "pale"},
@@ -51,9 +52,8 @@ int main(void) {
 		{"pale", "bake"}
 	});
 
-	for (int i = 0; i < inputs.size(); ++i) {
-		cout << inputs[i].first << ", " << inputs[i].second << ": " << solution.oneAway(inputs[i].first, inputs[i].second) << endl;
-	}
+	for (auto i : inputs)
+		cout << i.first << ", " << i.second << ": " << oneAway(i.first, i.second) << endl;
 
 	return 0;
 }
