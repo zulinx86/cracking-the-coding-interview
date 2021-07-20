16f484f99916bf5504d7b1e4a2ffbcdabb7e8403
zulinx86 <zulinx86@gmail.com>
2021-07-20 06:05:42 +0000

    refactoring

--- 01_arrays_and_strings/07_rotate_matrix/ans1.cpp
+++ 01_arrays_and_strings/07_rotate_matrix/ans1.cpp
@@ -1,43 +1,40 @@
 /*
- * Handle each layer
- * Time complexity: O(n)
+ * N: the number of elements of the given matrix
+ * Time complexity: O(N)
  * Space complexity: O(1)
  */
 
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-class Solution {
-public:
-	void rotateMatrix(vector<vector<int>> &mat) {
-		int n = mat.size();
+void rotateMatrix(vector<vector<int>> &mat) {
+	int n = mat.size();
 
-		for (int l = 0; l < n / 2; ++l) {
-			for (int i = 0; i < (n - 1) - 2 * l; ++i) {
-				int tmp;
-				tmp                                 = mat[l][l + i];
-				mat[l][l + i]                       = mat[(n - 1) - (l + i)][l];
-				mat[(n - 1) - (l + i)][l]           = mat[(n - 1) - l][(n - 1) - (l + i)];
-				mat[(n - 1) - l][(n - 1) - (l + i)] = mat[l + i][(n - 1) - l];
-				mat[l + i][(n - 1) - l]             = tmp;
-			}
+	for (int l = 0; l < n / 2; ++l) {
+		for (int i = 0; i < (n - 1) - 2 * l; ++i) {
+			int tmp;
+			tmp                                 = mat[l][l + i];
+			mat[l][l + i]                       = mat[(n - 1) - (l + i)][l];
+			mat[(n - 1) - (l + i)][l]           = mat[(n - 1) - l][(n - 1) - (l + i)];
+			mat[(n - 1) - l][(n - 1) - (l + i)] = mat[l + i][(n - 1) - l];
+			mat[l + i][(n - 1) - l]             = tmp;
 		}
 	}
+}
 
-	void showMatrix(const vector<vector<int>> &mat) {
-		for (int i = 0; i < mat.size(); ++i) {
-			for (int j = 0; j < mat[i].size(); ++j) {
-				printf("%02d ", mat[i][j]);
-			}
-			printf("\n");
+void showMatrix(const vector<vector<int>> &mat) {
+	for (int i = 0; i < mat.size(); ++i) {
+		for (int j = 0; j < mat[i].size(); ++j) {
+			printf("%02d ", mat[i][j]);
 		}
 		printf("\n");
 	}
-};
+	printf("\n");
+}
+
 
 int main(void) {
-	Solution solution;
 	vector<vector<vector<int>>> inputs({
 		{
 			{ 0,  1,  2,  3,  4},
@@ -54,14 +51,14 @@ int main(void) {
 		}
 	});
 
-	for (int i = 0; i < inputs.size(); ++i) {
+	for (auto i : inputs) {
 		cout << "input: " << endl;
-		solution.showMatrix(inputs[i]);
+		showMatrix(i);
 
-		solution.rotateMatrix(inputs[i]);
+		rotateMatrix(i);
 
 		cout << "output: " << endl;
-		solution.showMatrix(inputs[i]);
+		showMatrix(i);
 	}
 
 	return 0;
