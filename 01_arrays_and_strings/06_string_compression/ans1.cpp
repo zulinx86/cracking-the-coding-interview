885811b12867cb575e7a5d9752b690cbddb51a6d
zulinx86 <zulinx86@gmail.com>
2021-07-20 05:59:01 +0000

    add 1.6 String Compression

---
+++ 01_arrays_and_strings/06_string_compression/ans1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string stringCompression(const string &str) {
+	int len = 0;
+
+	for (int i = 0; i < str.length(); ++i) {
+		char c = str[i];
+		int cnt = 1;
+
+		while (i + 1 != str.length() && str[i + 1] == c) {
+			++cnt;
+			++i;
+		}
+
+		int len_cnt = 1;
+		while (cnt /= 10)
+			++len_cnt;
+
+		len += len_cnt + 1;
+	}
+
+	if (len < str.length()) {
+		string result;
+		result.reserve(len);
+
+		for (int i = 0; i < str.length(); ++i) {
+			char c = str[i];
+			int cnt = 1;
+
+			while (i + 1 != str.length() && str[i + 1] == c) {
+				++cnt;
+				++i;
+			}
+
+			result += c + to_string(cnt);
+		}
+
+		return result;
+	} else {
+		return str;
+	}
+}
+
+
+int main(void) {
+	vector<string> inputs({
+		"aabcccccaaa", /* a2b1c5a3 */
+		"aaaa", /* a4 */
+		"abcd", /* abcd (a1b1c1d1) */
+		"aaaaaaaaaaabbbbbbbbbbbbbb" /* a11b14 */
+	});
+
+	for (auto i : inputs)
+		cout << i << ": " << stringCompression(i) << endl;
+
+	return 0;
+}
