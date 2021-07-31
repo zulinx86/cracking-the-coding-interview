ba79d7be19a770b07908d2b56b2ab979dcf375c5
zulinx86 <zulinx86@gmail.com>
2021-07-31 01:18:09 +0000

    check the length of the input strings

--- 01_arrays_and_strings/02_check_permutation/ans1.cpp
+++ 01_arrays_and_strings/02_check_permutation/ans1.cpp
@@ -12,6 +12,9 @@
 using namespace std;
 
 bool checkPermutation(string& s1, string& s2) {
+	if (s1.length() != s2.length())
+		return false;
+
 	sort(s1.begin(), s1.end());
 	sort(s2.begin(), s2.end());
 	return s1 == s2;
