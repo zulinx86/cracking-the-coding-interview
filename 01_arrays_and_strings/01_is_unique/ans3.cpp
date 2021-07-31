5da70e005e5f8ffebd10f9debb90eecc1442cea7
zulinx86 <zulinx86@gmail.com>
2021-07-31 01:11:12 +0000

    add ans3 which uses a bit vector

---
+++ 01_arrays_and_strings/01_is_unique/ans3.cpp
@@ -0,0 +1,30 @@
+/*
+ * N: the length of the given string
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ */
+
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+bool isUnique(const string& str) {
+	int checker = 0;
+	for (int i = 0; i < str.length(); ++i) {
+		if (checker & (1 << str[i])) return false;
+		checker |= (1 << str[i]);
+	}
+
+	return true;
+}
+
+int main(void) {
+	vector<string> inputs({"hello", "world"});
+
+	for (int i = 0; i < inputs.size(); ++i)
+		cout << inputs[i] << ": " << isUnique(inputs[i]) << endl;
+
+	return 0;
+}
