b9c7bd8d9e57e6442356b0eda92066af3f020e54
zulinx86 <zulinx86@gmail.com>
2021-07-31 11:11:28 +0000

    add 2.3 Delete Middle Node

---
+++ 02_linked_lists/03_delete_middle_node/ans1.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+	int data;
+	struct Node *next;
+};
+
+void deleteMiddleNode(struct Node *n) {
+	struct Node *next = n->next;
+	n->data = next->data;
+	n->next = next->next;
+	delete next;
+}
+
+void showList(struct Node *n) {
+	cout << "[ ";
+	while (n != NULL) {
+		cout << n->data << " ";
+		n = n->next;
+	}
+	cout << "]" << endl;
+}
+
+int main(void)
+{
+	struct Node *head = NULL, *tail = NULL;
+	for (int i = 0; i < 10; ++i) {
+		struct Node *n = new struct Node;
+		n->data = i;
+		n->next = NULL;
+
+		if (head == NULL)
+			head = n;
+		else
+			tail->next = n;
+
+		tail = n;
+	}
+
+	cout << "input:" << endl;
+	showList(head);
+
+	deleteMiddleNode(head->next->next);
+
+	cout << "output:" << endl;
+	showList(head);
+
+	return 0;
+}
